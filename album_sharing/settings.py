151947671c6ca45394b3dd112633b5574e1e1aa2
saadmk11 <saad.mk11@hotmail.com>
2018-03-31 19:43:13 +0600

    static files added

--- album_sharing/settings.py
+++ album_sharing/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
 MEDIA_URL = "/media/"
 # Media File Storage
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
